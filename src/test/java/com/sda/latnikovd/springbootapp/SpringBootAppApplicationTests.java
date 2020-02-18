0c77ac114c870079ff9834f356d2fdd101389aca
Dmitry <latnikov@gmail.com>
2020-02-18 23:41:37 +0200

    Initial commit

---
+++ src/test/java/com/sda/latnikovd/springbootapp/SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.latnikovd.springbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
