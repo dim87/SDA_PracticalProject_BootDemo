de2d9ae22d6c33c88e210cdeebb8dded142ce43b
Dmitry <latnikov@gmail.com>
2020-02-21 22:44:19 +0200

    Added apache commons-lang

--- src/main/java/com/sda/latnikovd/springbootapp/modules/authors/AuthorsService.java
+++ src/main/java/com/sda/latnikovd/springbootapp/modules/authors/AuthorsService.java
@@ -2,6 +2,7 @@ package com.sda.latnikovd.springbootapp.modules.authors;
 
 import java.util.List;
 
+import org.apache.commons.lang3.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,6 +24,10 @@ public class AuthorsService {
 		if (author.getName() == null) {
 			throw new RuntimeException("name is undefined");
 		}
+
+		// example validate usages
+		Validate.notNull(author, "author is undefined");
+		Validate.notBlank(author.getName(), "name is blank for author '%s'", author);
 
 		return authorRepository.save(author);
 	}
